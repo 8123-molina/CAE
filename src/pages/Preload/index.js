653fb3b749340cb552f4b0c13a5e19f6b7d6c84f
8123-molina <60671190+8123-molina@users.noreply.github.com>
2021-12-18 17:00:11 -0300

    xxxx

--- src/pages/Preload/index.js
+++ src/pages/Preload/index.js
@@ -4,7 +4,7 @@ import {Container, TextPrymari} from '../../styles/styles'
 export default () => {
     return(
         <Container>
-            <TextPrymari>TesxtePreload</TextPrymari>
+            <TextPrymari>TesxtxxxxxxxePreload</TextPrymari>
 
         </Container>
     )
